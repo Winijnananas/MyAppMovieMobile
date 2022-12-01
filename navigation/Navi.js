3a2f6ee1d8535c0cfa685f915e97bc0eb92bbfa6
Winijnananas <winijnan.ji@mail.wu.ac.th>
2022-12-01 23:23:16 +0700

    Update package ,loading Screen

--- navigation/Navi.js
+++ navigation/Navi.js
@@ -13,6 +13,7 @@ import MainContainer from './MainContainer';
 import RegisterScreen from './screens/RegisterScreen';
 import UserScreen from './screens/UserScreen';
 import DetailsMovieScreen from './screens/DetailsMovieScreen';
+// import SplashScreen from './screens/SplashScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -71,7 +72,9 @@ const Navigation = () => {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}} >
             {/* <Stack.Screen name="Welcome" component={}/> */}
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            {/* <Stack.Screen name="Splash" component={SplashScreen} /> */}
+
+          {/* <Stack.Screen name="Welcome" component={WelcomeScreen} /> */}
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Main" component={MainContainer}/>
           <Stack.Screen name="Regis" component={RegisterScreen}/>
