98d6bbb80b989a6fcccda5468c4a8c4eadbd020e
Winijnananas <winijnan.ji@mail.wu.ac.th>
2022-12-15 13:30:09 +0700

    ipconfig

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -14,7 +14,7 @@ export default function RegisterScreen({ navigation }) {
 
 
     
-    const API = "http://172.16.156.81:3000/users";
+    const API = "http://192.168.132.1:3000/users";
     // const API = "http://127.0.0.1:3000/users";
     const register = () => {
         if(!username || !email || !password ) {
