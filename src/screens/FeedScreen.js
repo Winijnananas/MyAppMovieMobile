274537252c9693a56d19ab8e80a397c3f1f91ecb
Winijnananas <winijnan.ji@mail.wu.ac.th>
2022-12-20 21:36:16 +0700

    Feed test APi part 2

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,13 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image,StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function FeedScreen() {
   return (
     <View
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      style={styles.item}>
         
+        <Image 
+        style={styles.poster}
+
+        source={{
+          uri:"http://image.tmdb.org/t/p/w342" + props.item.poster_path,
+        }}
+        />
       <Text style={{fontSize:30,fontWeight:'bold'}}>FEED SCREEN👋</Text>
   
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  item:{
+    flexDirection:"column",
+    flexWrap:"wrap",
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  poster:{
+    width:170,
+    height:255,
+    borderRadius:10,
+    marginBottom:10,
+  }
+})
